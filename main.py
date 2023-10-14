19c3d5ba68e5907a2024032db04ecb1184df683f
Benedek Sandor <benedekgordonsandor@gmail.com>
2023-10-13 20:04:20 -0700

    switched to selenium as bs doesnt work with dynamic sites

--- main.py
+++ main.py
@@ -1,28 +1,73 @@
-import requests
 import pandas as pd
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+from datetime import datetime
 
 url = 'https://efdsearch.senate.gov/search/'
 
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-csrf_token = soup.find('input', {'name': 'csrfmiddlewaretoken'}).get('value')
-csrf_cookie = response.cookies['csrftoken']
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
-    'Referer': url,
-}
-cookies = {
-    'csrftoken': csrf_token
-}
-payload = {
-    'prohibition_agreement': '1',
-    'csrfmiddlewaretoken': csrf_token
-}
-response = requests.post(url, headers=headers, data=payload, cookies=cookies)
-if response.status_code == 200:
-    print("Checkbox checked successfully.")
-else:
-    print("Failed to check the checkbox.")
-soup = BeautifulSoup(response.text, 'html.parser')
-print(soup.prettify())
+driver = webdriver.Firefox()
+driver.get('https://efdsearch.senate.gov/search/')
+checkbox = driver.find_element(By.ID, 'agree_statement')
+checkbox.click()
+
+search = driver.find_element(By.ID, 'search_options')
+
+#check current and previous senators
+current_senators = search.find_element(By.XPATH, '//label[input[@value="1"]]')
+current_senators.click()
+former_senators = search.find_element(By.XPATH, '//label[input[@value="5"]]')
+former_senators.click()
+
+#check type of reports (periodic)
+periodic_reports = search.find_element(By.XPATH, '//label[input[@value="11"]]')
+periodic_reports.click()
+
+#from beginning of year till now
+start = '01/01/2023'
+end = datetime.now().date()
+from_date = search.find_element(By.ID, 'fromDate')
+from_date.send_keys(start)
+
+#submit form
+submit = search.find_element(By.XPATH, '/html/body/div[1]/main/div/div/div[5]/div/form/div/button')
+submit.click()
+
+wait = WebDriverWait(driver, 10) 
+wait.until(EC.presence_of_all_elements_located((By.XPATH, '//*[@id="filedReports"]/tbody/tr')))
+
+#get actual data
+table = driver.find_element(By.ID, 'filedReports')
+rows = table.find_elements(By.TAG_NAME, "tr")
+main_window = driver.current_window_handle
+data = []
+for row in rows[1:]:
+    row.find_element(By.TAG_NAME, "a").click()
+    columns = row.find_elements(By.TAG_NAME, "td")
+    first_name = columns[0].text.capitalize()
+    last_name = columns[1].text.capitalize()
+    senator = first_name + ' ' + last_name
+    driver.switch_to.window(driver.window_handles[1])
+    try: 
+        electronic = WebDriverWait(driver, 1).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'mb-4')))
+        cols = driver.find_elements(By.TAG_NAME, "td")
+        data.append({
+            "Stock": cols[3].text.strip() if cols[3].text.strip() != '--' else ''.join(cols[4].text.strip().split('\n')[0]),
+            "Transaction Date": cols[1].text.strip(),
+            "Buy/Sell": ''.join(cols[6].text.strip().split(' ')[0]),
+            "Type": cols[5].text.strip(),
+            "Amount": cols[7].text.strip(),
+            "Senator": senator
+        })
+    except:
+        print('NOT ELECTRONIC')
+    driver.close()
+    driver.switch_to.window(main_window)
+
+time.sleep(5)
+driver.quit()
+columns = ['Stock', 'Transaction Date', 'Buy/Sell', 'Type', 'Amount', 'Senator']
+dataframe = pd.DataFrame(data, columns=columns)
+print(dataframe)
