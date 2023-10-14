726f4cc1521ad4665ac44d7a9b0c400f6e2b3831
Benedek Sandor <benedekgordonsandor@gmail.com>
2023-10-14 12:09:03 -0700

    working on creating dataframe; playing with time so it can load everything

--- main.py
+++ main.py
@@ -37,37 +37,50 @@ submit.click()
 
 wait = WebDriverWait(driver, 10) 
 wait.until(EC.presence_of_all_elements_located((By.XPATH, '//*[@id="filedReports"]/tbody/tr')))
+columns = ['Stock', 'Transaction Date', 'Buy/Sell', 'Type', 'Amount', 'Senator']
+df = pd.DataFrame(columns=columns)
 
-#get actual data
-table = driver.find_element(By.ID, 'filedReports')
-rows = table.find_elements(By.TAG_NAME, "tr")
-main_window = driver.current_window_handle
-data = []
-for row in rows[1:]:
-    row.find_element(By.TAG_NAME, "a").click()
-    columns = row.find_elements(By.TAG_NAME, "td")
-    first_name = columns[0].text.capitalize()
-    last_name = columns[1].text.capitalize()
-    senator = first_name + ' ' + last_name
-    driver.switch_to.window(driver.window_handles[1])
-    try: 
-        electronic = WebDriverWait(driver, 1).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'mb-4')))
-        cols = driver.find_elements(By.TAG_NAME, "td")
-        data.append({
-            "Stock": cols[3].text.strip() if cols[3].text.strip() != '--' else ''.join(cols[4].text.strip().split('\n')[0]),
-            "Transaction Date": cols[1].text.strip(),
-            "Buy/Sell": ''.join(cols[6].text.strip().split(' ')[0]),
-            "Type": cols[5].text.strip(),
-            "Amount": cols[7].text.strip(),
-            "Senator": senator
-        })
-    except:
-        print('NOT ELECTRONIC')
-    driver.close()
-    driver.switch_to.window(main_window)
+while True:
+    #get actual data
+    table = driver.find_element(By.ID, 'filedReports')
+    rows = table.find_elements(By.TAG_NAME, "tr")
+    main_window = driver.current_window_handle
+    for row in rows[1:]:
+        row.find_element(By.TAG_NAME, "a").click()
+        columns = row.find_elements(By.TAG_NAME, "td")
+        first_name = columns[0].text.capitalize()
+        last_name = columns[1].text.capitalize()
+        senator = first_name + ' ' + last_name
+        driver.switch_to.window(driver.window_handles[1])
+        try: 
+            electronic = WebDriverWait(driver, 2).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'mb-4')))
+            rows = driver.find_elements(By.TAG_NAME, "tr")
+            for row in rows[1:]:
+                cols = row.find_elements(By.TAG_NAME, "td")
+                df = df.append({
+                    "Stock": cols[3].text.strip() if cols[3].text.strip() != '--' else cols[4].text.strip(),
+                    "Transaction Date": cols[1].text.strip(),
+                    "Buy/Sell": ''.join(cols[6].text.strip().split(' ')[0]),
+                    "Type": cols[5].text.strip(),
+                    "Amount": cols[7].text.strip(),
+                    "Senator": senator
+                }, ignore_index=True)
+            time.sleep(2)
+        except:
+            pass
+        driver.close()
+        driver.switch_to.window(main_window)
+    next_button = driver.find_element(By.ID, 'filedReports_next')
+    if 'disabled' in next_button.get_attribute('class'):
+        break
+    next_button.click()
+    time.sleep(2)
+    wait.until(EC.presence_of_all_elements_located((By.XPATH, '//*[@id="filedReports"]/tbody/tr')))
 
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+print(df)
 time.sleep(5)
 driver.quit()
-columns = ['Stock', 'Transaction Date', 'Buy/Sell', 'Type', 'Amount', 'Senator']
-dataframe = pd.DataFrame(data, columns=columns)
-print(dataframe)
+#df.to_csv('data.csv', index=False) 
